420608a105545ff9c37120236272afa6a74bd589
Vinci4600 <vincent.diergardt@gmail.com>
2026-06-09 22:29:22 +0200

    Verbesserung Rechtschreibe Fehler Home.jsx

--- frontend/Praktikums_web/praktikum-web/src/pages/Home.jsx
+++ frontend/Praktikums_web/praktikum-web/src/pages/Home.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from 'react-router-dom';
 function Home() {
     return (
         <div><h1>Hier entsteht das Home Verzeichnis </h1>
-        <marquee><h2>Domink's Part</h2></marquee></div>
+        <marquee><h2>Dominik's Part</h2></marquee></div>
     )
 }
 

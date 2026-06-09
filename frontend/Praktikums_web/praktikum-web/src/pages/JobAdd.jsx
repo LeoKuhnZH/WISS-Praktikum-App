d4c2c903a458a26b36e92f33fdd835074357573c
Vinci4600 <vincent.diergardt@gmail.com>
2026-06-09 09:07:59 +0200

    add JobAdd.jsx

---
+++ frontend/Praktikums_web/praktikum-web/src/pages/JobAdd.jsx
@@ -0,0 +1,13 @@
+import './components/style/style.css';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+
+function JobAdd() {
+    return (
+        <div><h1>Hier entsteht das JobAdd Verzeichnis </h1>
+        <marquee><h2>Indujan's Part</h2></marquee></div>
+    )
+}
+
+export default JobAdd;

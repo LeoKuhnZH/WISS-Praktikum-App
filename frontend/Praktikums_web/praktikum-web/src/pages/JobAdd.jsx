5ee50acee1cd006d17857aca75babd86eca0f3ff
IndujanMF <Indujanv.030108@gmail.com>
2026-06-09 02:32:18 -0700

    Hallo

--- frontend/Praktikums_web/praktikum-web/src/pages/JobAdd.jsx
+++ frontend/Praktikums_web/praktikum-web/src/pages/JobAdd.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 
 function JobAdd() {
     return (
-        <div><h1>Hier entsteht das JobAdd Verzeichnis </h1>
+        <div><h1>Hier entsteht das Job Add Verzeichnis </h1>
         <marquee><h2>Indujan's Part</h2></marquee></div>
     )
 }

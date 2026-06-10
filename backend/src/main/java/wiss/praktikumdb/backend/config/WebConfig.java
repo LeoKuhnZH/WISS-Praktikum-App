a9d0b94fc2d65eb5cfca5db22b53bd6a80b82c1d
LeoKuhnZH <leo.kuhn.zh@gmail.com>
2026-06-10 11:26:17 +0200

    made the WebConfig.java

--- backend/src/main/java/wiss/praktikumdb/backend/config/WebConfig.java
+++ backend/src/main/java/wiss/praktikumdb/backend/config/WebConfig.java
@@ -1,4 +1,26 @@
 package wiss.praktikumdb.backend.config;
 
-public class WebConfig {
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    private static final String URL = "http://localhost:5173";
+    private static final String[] ALLOWED_METHODS = new String[]{"GET", "POST", "PUT", "DELETE", "OPTIONS"};
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/posting/**")
+                .allowedOrigins(URL)
+                .allowedMethods(ALLOWED_METHODS)
+                .allowedHeaders("*")
+                .allowCredentials(false)
+                .maxAge(3600);
+        registry.addMapping("/api/auth/**")
+                .allowedOrigins(URL)
+                .allowedMethods(ALLOWED_METHODS)
+                .allowedHeaders("*")
+                .allowCredentials(false)
+                .maxAge(3600);
+    }
 }

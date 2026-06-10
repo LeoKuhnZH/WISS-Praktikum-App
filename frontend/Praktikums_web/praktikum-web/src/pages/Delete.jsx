496f9da6026ad9ab90120b8ad2989a8080cb965c
Vinci4600 <vincent.diergardt@gmail.com>
2026-06-10 11:15:12 +0200

    add function delet in Delete.jsx

---
+++ frontend/Praktikums_web/praktikum-web/src/pages/Delete.jsx
@@ -0,0 +1,10 @@
+import './components/style/style.css';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const API_BASE = 'http://localhost:8080/api/posting';
+function Delete() {
+    const navigate = useNavigate();
+
+
+}
